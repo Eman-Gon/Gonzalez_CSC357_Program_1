bdec80b742ebda096d7d134238cdde8b67f8edf6
egonz279 <egonz279@calpoly.edu>
2024-03-14 17:40:36 -0700

    Regrade

--- word_count.c
+++ word_count.c
@@ -2,36 +2,48 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main() {
-    char line[255]; 
-    FILE *fp = fopen("test.txt", "r"); 
-    int lines = 0, chars = 0, words = 0, counter = 0; 
-    if (fp == NULL) {
-        printf("error\n"); 
-        return 1;
+#define MAX_LINE_LENGTH 1000
+
+int main(int argc, char *argv[]) {
+    FILE *fp = stdin;
+    char line[MAX_LINE_LENGTH];
+    int lines = 0, chars = 0, words = 0, counter = 0;
+
+    if (argc > 1) {
+        fp = fopen(argv[1], "r");
+        if (fp == NULL) {
+            fprintf(stderr, "No file %s\n", argv[1]);
+            return 1;
+        }
     }
 
-    while (fgets(line, sizeof(line), fp)) { 
-        for (int i = 0; line[i] != '\0'; i++) { 
-            chars++; 
-            if (isspace(line[i]) || line[i] == '\n') { 
-           if (counter) { 
-            words++; 
-            counter = 0; 
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        for (int i = 0; line[i] != '\0'; i++) {
+        chars++;
+            if (isspace(line[i]) || line[i] == '\n') {
+        if (counter) {
+               words++;
+                    counter = 0;
                 }
             } else {
-                counter = 1; 
+                counter = 1;
             }
         }
-        if (counter) { 
-        words++; 
-        counter = 0;
+        if (counter) {
+                   words++;
+            counter = 0;
         }
-        lines++; 
     }
+
+    if (chars > 0) {
+        lines = 1;
+    }
+
     printf("%d\t%d\t%d\n", lines, words, chars);
 
-    fclose(fp); 
+    if (fp != stdin) {
+        fclose(fp);
+    }
+
     return 0;
 }
-
